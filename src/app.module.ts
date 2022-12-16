8ec4d8023fa2f0559869de4e126a25ffcb857a92
apolo <valbuenafelipejuan13@gmail.com>
2022-12-16 14:52:59 -0500

    creando recursos de auth, events y users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, EventsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
